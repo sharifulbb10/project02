9a7fe9d7e5b750a12c5fb51379a391f9c0011bff
sharifulbb10 <sharifulbb10@gmail.com>
2025-03-04 21:03:54 +0600

    further modification

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/project01/",
+  base: "/project02/",
   plugins: [react(), tailwindcss()],
 })
